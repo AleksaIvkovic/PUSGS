75eb6474c8fcc1323758e9ab530550b7238f7bb2
crystalboy97 <58144983+crystalboy97@users.noreply.github.com>
2020-04-06 19:08:24 +0200

    mapa dodata

--- careoplane/src/app/components/main/airlines/airline-details/airline-details.component.ts
+++ careoplane/src/app/components/main/airlines/airline-details/airline-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormsModule } from '@angular/forms'; 
+/// <reference types=”@types/googlemaps” />
 
 import { Airline } from 'src/app/models/airline.model';
 import { AirlineService } from 'src/app/services/airline.service';
@@ -10,7 +11,7 @@ import { AirlineService } from 'src/app/services/airline.service';
   templateUrl: './airline-details.component.html',
   styleUrls: ['./airline-details.component.css']
 })
-export class AirlineDetailsComponent implements OnInit {
+export class AirlineDetailsComponent implements OnInit, AfterViewInit {
   id: number;
   airline: Airline;
   
@@ -20,6 +21,20 @@ export class AirlineDetailsComponent implements OnInit {
   distance : number;
   connections : number;
   price : number;
+
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.730610;
+  lng = -73.935242;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8,
+  };
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
 
   constructor(private activeRoute: ActivatedRoute, private airlineService: AirlineService) { }
 
@@ -32,5 +47,15 @@ export class AirlineDetailsComponent implements OnInit {
     );
   }
 
-  
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, 
+    this.mapOptions);
+    this.marker.setMap(this.map);
+   }
+
+   ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+
 }
