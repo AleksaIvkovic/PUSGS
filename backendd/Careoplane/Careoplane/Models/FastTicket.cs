d51a95fcb0438afb9674528200b8429e3b9e2c21
crystalboy97 <58144983+crystalboy97@users.noreply.github.com>
2020-05-07 11:26:19 +0200

    doradjen backend

---
+++ backendd/Careoplane/Careoplane/Models/FastTicket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Careoplane.Models
+{
+    public class FastTicket
+    {
+        [Key]
+        public int FastTicketId { get; set; }
+
+        public double NewPrice { get; set; }
+
+        public Seat Seat { get; set; }
+
+        public Flight Flight { get; set; }
+    }
+}
